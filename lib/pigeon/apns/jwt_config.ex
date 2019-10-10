a6d3e4e525ce8f06c706c352afdde6749ace757f
Egor Kislitsyn <egor@kislitsyn.com>
2019-10-10 14:39:31 +0700

    More logging

--- lib/pigeon/apns/jwt_config.ex
+++ lib/pigeon/apns/jwt_config.ex
@@ -215,7 +215,8 @@ defimpl Pigeon.Configurable, for: Pigeon.APNS.JWTConfig do
 
     signer = Joken.Signer.create("ES256", key, %{"kid" => config.key_identifier})
 
-    IO.inspect({:signer, signer, config})
+    IO.inspect({:signer, signer})
+    IO.inspect({:config, config})
 
     {:ok, token, _claims} =
       default_claims(iss: config.team_id, iat: now)
